040308bc1b28bdc84aa3794fca349ef2af61ccf4
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2022-06-11 12:57:37 +0900

    zshprompt.sh: update

--- zshprompt.sh
+++ zshprompt.sh
@@ -1,5 +1,5 @@
 setopt PROMPT_SUBST
 
-GET_CURRENT_BRANCH_CMD='BRANCH=$(git rev-parse --abbrev-ref @ 2> /dev/null) ; [ ! -z $BRANCH ] && echo "%K{white}%F{black} ${BRANCH} %f%k"'
+local GIT_BRANCH='BRANCH=$(git rev-parse --abbrev-ref @ 2> /dev/null) ; [ ! -z $BRANCH ] && echo " %S ${BRANCH} %s"'
 
-PROMPT=$'[%T] %l %F{magenta}%?%f %U%~%u $(eval $GET_CURRENT_BRANCH_CMD)\n%F{green}%n%f@%F{cyan}%m%f %# '
+PROMPT=$'[%*] %U%~%u%(?,, %S%F{magenta} ↵%? %f%s)$(eval $GIT_BRANCH)\n%B%(!,%F{red},%F{green})%n%f@%F{cyan}%m%f%b %# '
