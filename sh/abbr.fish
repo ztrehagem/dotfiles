9da1d2b700a727a67de0df7254e72111f1700607
Shigeki Suwa <shigeki.suwa@oro.com>
2023-01-25 12:53:30 +0900

    fish abbr pnpm

--- sh/abbr.fish
+++ sh/abbr.fish
@@ -5,6 +5,8 @@ abbr -a -g n npm
 abbr -a -g nr npm run
 abbr -a -g nrp npm run --prefix
 abbr -a -g ns npm start
+abbr -a -g p pnpm
+abbr -a -g pr pnpm run
 abbr -a -g d docker
 abbr -a -g dc docker compose
 abbr -a -g v vagrant
