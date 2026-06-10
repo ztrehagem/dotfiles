17372ec0c371b103689976d41d7b3920a24e5dbb
SUWA Shigeki <shigeki.suwa@oro.com>
2026-06-10 16:33:40 +0900

    update

--- shared/fish/config.fish
+++ shared/fish/config.fish
@@ -4,8 +4,8 @@ end
 
 if status is-login
 	source ~/.dotfiles/shared/fish/env.fish
-	source ~/.dotfiles/shared/fish/brew.fish
 	source ~/.dotfiles/shared/fish/asdf.fish
+	source ~/.dotfiles/shared/fish/brew.fish
 	source ~/.dotfiles/shared/fish/direnv.fish
 	source ~/.dotfiles/shared/fish/android-mac.fish
 	source ~/.dotfiles/shared/fish/abbr.fish
